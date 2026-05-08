93d72cc641c6488f6db679aad1ad764054040949
Serghei Ostapenco <misterdi0320@gmail.com>
2026-05-08 14:29:53 +0300

    изменение папки Tourscontroller

--- Controllers/ToursController.cs
+++ Controllers/ToursController.cs
@@ -1,5 +1,7 @@
+using Backend.Data;
+using Backend.Models;
 using Microsoft.AspNetCore.Mvc;
-using Backend.Services;
+using Microsoft.EntityFrameworkCore;
 
 namespace Backend.Controllers;
 
@@ -7,19 +9,69 @@ namespace Backend.Controllers;
 [Route("api/tours")]
 public class ToursController : ControllerBase
 {
-    private readonly ITourService _tourService;
+    private readonly AppDbContext _context;
 
-
-    public ToursController(ITourService tourService)
+    public ToursController(AppDbContext context)
     {
-        _tourService = tourService;
+        _context = context;
     }
 
     [HttpGet]
-    public IActionResult Get()
+    public async Task<IActionResult> Get()
     {
-        
-        var tours = _tourService.GetAllTours();
+        var tours = await _context.Tours.ToListAsync();
         return Ok(tours);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetById(int id)
+    {
+        var tour = await _context.Tours.FindAsync(id);
+
+        if (tour == null)
+            return NotFound();
+
+        return Ok(tour);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(Tour tour)
+    {
+        _context.Tours.Add(tour);
+        await _context.SaveChangesAsync();
+
+        return Ok(tour);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Update(int id, Tour updatedTour)
+    {
+        var tour = await _context.Tours.FindAsync(id);
+
+        if (tour == null)
+            return NotFound();
+
+        tour.Title = updatedTour.Title;
+        tour.Price = updatedTour.Price;
+        tour.Category = updatedTour.Category;
+        tour.Img = updatedTour.Img;
+
+        await _context.SaveChangesAsync();
+
+        return Ok(tour);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete(int id)
+    {
+        var tour = await _context.Tours.FindAsync(id);
+
+        if (tour == null)
+            return NotFound();
+
+        _context.Tours.Remove(tour);
+        await _context.SaveChangesAsync();
+
+        return Ok();
     }
 }
