f1a3491c13b69e3e5b56841670385121fe2d6fcb
alexeipavliuc <alekseipavluk10@gmail.com>
2026-05-14 23:38:43 +0300

    Добавили фильтры доступа для ролей

--- Filters/UserModAttribute.cs
+++ Filters/UserModAttribute.cs
@@ -3,18 +3,20 @@ using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace Backend;
 
-public class AdminModAttribute : ActionFilterAttribute
+public class UserModAttribute : ActionFilterAttribute
 {
     public override void OnActionExecuting(ActionExecutingContext context)
     {
         var userRole = context.HttpContext.Request.Headers["Role"].ToString();
+        var isUser = string.Equals(userRole, "User", StringComparison.OrdinalIgnoreCase);
+        var isAdmin = string.Equals(userRole, "Admin", StringComparison.OrdinalIgnoreCase);
 
-        if (!string.Equals(userRole, "Admin", StringComparison.OrdinalIgnoreCase))
+        if (!isUser && !isAdmin)
         {
             context.Result = new ContentResult()
             {
                 StatusCode = 403,
-                Content = "Forbidden: Admin role required"
+                Content = "Forbidden: User or Admin role required"
             };
         }
 
